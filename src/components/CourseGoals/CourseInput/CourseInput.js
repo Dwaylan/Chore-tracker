696c033625fd2790b5fcc2c65af903998370e4a7
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-11-11 17:12:21 -0500

    working on inline styling

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,38 @@ import "./CourseInput.css";
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = (event) => {
     setEnteredValue(event.target.value);
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
+    // If the entered value is 0 characters set the validation to falsey
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>To do list</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        {/* If there is a falsey (aka zero input value the label is styled red, else black) */}
+        <label style={{ color: !isValid ? "red" : "black" }}>To do list</label>
+        <input
+          style={{
+            borderColor: !isValid ? "red" : "black",
+            background: !isValid ? "salmon" : "transparent",
+          }}
+          type="text"
+          onChange={goalInputChangeHandler}
+        />
       </div>
       <Button type="submit">Add</Button>
     </form>
